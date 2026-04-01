28f84c8dc9338899910aa3e5f61b737217b8dc58
bhatiabh-29 <bhavinbhatia4@gmail.com>
2026-04-01 15:05:38 -0400

    Day3: Added Failed Login and Logout Tests - Locators deeep dive

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -16,6 +16,46 @@ test("Successfully Logged In", async  ({page}) =>{
 
     //Assert that the user is successfully logged in by checking for the presence of a logout button and Text Secure Area
     await expect(page.getByRole('heading', {name: 'Secure Area', exact: true})).toBeVisible();
+})
+
+test("Failed Logged In", async ({page}) =>{
+
+    //navigate to the login page
+    await page.goto("https://the-internet.herokuapp.com/login");
+
+    //find the username and password field and enter incorrect details
+    await page.getByLabel('Username').fill("Invalidusername");
+    await page.getByLabel('Password').fill("InvalidPassword");
+
+    //find the login button and click on it
+    await page.getByRole("button", {name: "login"}).click();
+
+    //Aseert that the user is not logged in by checking for the presence of an error message
+    await expect(page.getByText("Your username is invalid!")).toBeVisible();
+})
+
+test("Logout", async ({page}) =>{
+
+    //navigate to the url
+    await page.goto("https://the-internet.herokuapp.com/login");
+
+    //Find Username and password field and enter correct details
+    await page.getByLabel('Username').fill('tomsmith');
+    await page.getByLabel('Password').fill("SuperSecretPassword!");
+
+    //find the login button and click on it
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    //Once the user is loggen in find the text " You logged into a secure area!" and assert that it is visible
+    await expect(page.getByText('You logged into a secure area!')).toBeVisible();
+
+    //Search for the logout button and click on it
+    await page.locator('i', {hasText: 'Logout'}).click();
+
+    //Asser that the user is successfully logged out by checking for the presence of the login button and text "You logged out of the secure area!"
+    await expect(page.getByText('You logged out of the secure area!')).toBeVisible();
+
 
 })
+
 
